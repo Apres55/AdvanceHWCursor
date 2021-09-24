f89e809f7f8bb519602103b6e2ccf8a314f242f3
Apres Azghaldian <azgaldian2@gmail.com>
2021-09-24 19:03:25 +0300

    added and deduced to html 11 equations + some css styles

--- HW#1/jsfile.js
+++ HW#1/jsfile.js
@@ -8,43 +8,52 @@ console.log(maxSum);
     let minSum = Math.min(whiteBread,redWine,meat);
 console.log(minSum);
 
-    let objectsSum = +(whiteBread+redWine+meat);
+    let objectsSum = whiteBread + redWine + meat;
 console.log(objectsSum);
 
-    let objIntegersSum = +(Math.floor(objectsSum));
-console.log(objectIntegersSum);
+    let objIntegersSum = Math.floor(whiteBread) + Math.floor(redWine) + Math.floor(meat);
+console.log(objIntegersSum);
 
-    function roundHundredSum(objectsSum){
-        return Math.round(objectsSum/100)*100}
+    let roundHundredSum = Math.round(objectsSum / 100) * 100;
 console.log(roundHundredSum);
 
-    let oddOrEven = roundOfSum % 2 == 0;
+    let oddOrEven = objIntegersSum % 2 === 0;
 console.log(oddOrEven);
+
+if(oddOrEven === true){
+    console.log("even");
+} else {
+    console.log("odd");
+}
 
     let delivery = 500 - objectsSum;
 console.log(delivery);
 
-    let avarageValue = (objectsSum/3)
+    let avarageValue = objectsSum / 3;
 console.log(avarageValue.toFixed(2));
 
-    const sale = Math.floor(Math.random() *100);
+    const sale = Math.floor(Math.random() * 100);
 
-    const salePrice = (objectsSum * sale) /100;
-console.log(salePrice.toFixed(2));
+    const salePrice = (objectsSum - sale).toFixed(2);
+console.log(salePrice);
 
-    const profit = salePrice-(objectsSum/2);
+    const profit = (objectsSum / 2) - sale;
 console.log(profit);
 
-document.writeln(`
-    Maximum number: ${maxSum}
-    Minimum number: ${minSum}
-    Sum: ${objectsSum}
-    Round down: ${objIntegersSum}
-    Rounding in hundreds: ${roundHundredSum}
-    BooleanValue: ${oddOrEven}
-    Change: ${delivery}
-    average price: ${avarageValue}
-    Random discount: ${sale}
-    Discount amount: ${salePrice}
-    Net profit: ${profit}
-    `);
+document.writeln(`<ul>
+<table>
+
+        <th><b>Maximum number:</b> ${maxSum} </th>
+        <th><b>Minimum number:</b> ${minSum} </th>
+        <th><b>Sum:</b> ${objectsSum} </th>
+        <th><b>Round down:</b> ${objIntegersSum} </th>
+        <th><b>Rounding in hundreds:</b> ${roundHundredSum} </th>
+        <th><b>BooleanValue:</b> ${oddOrEven === true} </th>
+        <th><b>After pay 500:</b> ${delivery} </th>
+        <th><b>Average price:</b> ${avarageValue} </th>
+        <th><b>Random discount:</b> ${sale} </th>
+        <th><b>Amount after discount:</b> ${salePrice} </th>
+        <th><b>Net profit:</b> ${profit} </th>
+
+    </table>
+    </ul>`);
