af5781d05e556993296c9f66c6c7bf2aca4ada58
Apres Azghaldian <azgaldian2@gmail.com>
2021-09-29 11:33:14 +0300

    download second hw - create 3 cycles and some ternary operators for check correctness numbers in console log

---
+++ HW2CyclesBranching/hw2js.js
@@ -0,0 +1,26 @@
+let N;
+let M;
+
+do{
+    N = parseInt(prompt("Enter first number"));
+} while (Number.isNaN(N));
+
+let checkFirstNum = N != NaN ? "Correct Number" : "Not Integer number or it's letters";
+
+do{
+    M = parseInt(prompt("Enter second number"));
+} while (Number.isNaN(M));
+
+let checkSecondNum = N != NaN ? "Correct Number" : "Not Integer number or it's letters";
+
+const declineEvenNumbers = confirm(`Skip even numbers?`);
+
+let sum = 0;
+    for (let i = N; i <= M; i++) {
+    if (declineEvenNumbers && (i % 2 === 0 )) {
+        continue;
+    }
+    sum += i;  
+}
+
+document.writeln (`<li>Result: ${sum}</li>`);
