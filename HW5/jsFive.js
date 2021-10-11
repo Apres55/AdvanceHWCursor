55a64c86102ef80e8b87f184eb88db54590572a7
Apres Azghaldian <azgaldian2@gmail.com>
2021-10-11 19:49:51 +0300

    reworked some tasks after support request

--- HW5/jsFive.js
+++ HW5/jsFive.js
@@ -1,9 +1,9 @@
 // 1 getRandomArray
 
-function getRandomArray (length, min, max) {
+function getRandomArray(length, min, max) {
 	let randomArr = [];
 		for (let i = 0; i < length; i++) {
-			randomArr.push(Math.floor(Math.random() * (max - min) + min));
+		randomArr.push(Math.floor(Math.random() * (max - min + 1) + min));
 	}
 		return randomArr;
 }
@@ -14,18 +14,19 @@ console.log(getRandomArray(10, 3, 99));
 
 const getModa = (...numbers) => {
     const frequency = {};
+	const key = 0;
+	const value = 1;
     numbers.forEach(number => {
-        if(!frequency[number]){
-            frequency[number] = 1
+        if(!frequency[number]) {
+        frequency[number] = 1
         }
-        else{
-            frequency[number] += 1;
+        else {
+        frequency[number] += 1;
         }
     });
     const maxFrequency = Math.max(...Object.values(frequency));
-    const moda = Object.entries(frequency).filter(item => item[1] === maxFrequency);
-
-    return moda.map(number => number[0]).concat();
+    const moda = Object.entries(frequency).filter(item => item[value] === maxFrequency);
+		return moda.map(number => number[key]);
 }
 console.log(getModa(6, 2, 55, 11, 78, 2, 55, 55, 77, 57, 87, 55, 23, 2, 56, 3, 2));
 
@@ -33,21 +34,17 @@ console.log(getModa(6, 2, 55, 11, 78, 2, 55, 55, 77, 57, 87, 55, 23, 2, 56, 3, 2
 
 function getAverage(...numbers) {
 	const avarage = numbers.filter(number => Number.isInteger(number))
-			return avarage.reduce((sum, number) => (sum + number)) / numbers.length;
+		.reduce((sum, number) => (sum + number)) / numbers.length;
+		return avarage;
 }
 
 console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
-
 // 4 getMedian(...numbers)
 
 function getMedian(...numbers) {
-	const sortNum = numbers.sort(function(a, b) {
-		return a - b;
-	});
-
-	const minOfMiddle = Math.floor((sortNum.length - 1) / 2);
-	const maxOfMiddle = Math.ceil((sortNum.length - 1) / 2);
-		return (sortNum[minOfMiddle] + sortNum[maxOfMiddle]) / 2;
+	const sortNum = numbers.sort((a, b) => (a - b));
+	const sortNumLenght = (sortNum.length - 1) / 2;
+		return (sortNum[Math.floor(sortNumLenght)] + sortNum[Math.ceil(sortNumLenght)]) / 2;
 }
 
 console.log(getMedian(1, 2, 3, 4, 5));
@@ -55,16 +52,14 @@ console.log(getMedian(1, 2, 3, 4, 5));
 // 5 filterEvenNumbers(...numbers)
 
 function filterEvenNumbers(...numbers) {
-	let result = numbers.filter(elem => elem % 2 !== 0);
-		return result
+	return numbers.filter(elem => elem % 2 !== 0);
 }
 console.log(filterEvenNumbers(1, 2, 3, 4, 5, 6)); 
 
 // 6 countPositiveNumbers(...numbers)
 
 function countPositiveNumbers(...numbers) {
-	const totalPositiveNum = numbers.filter(number => number > 0)
-		return totalPositiveNum.length
+	return numbers.filter(number => number > 0).length;
 }
 console.log(countPositiveNumbers(1, -2, 3, -4, -5, 6)); 
 
