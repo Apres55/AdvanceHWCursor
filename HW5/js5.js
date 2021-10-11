e2e290dcaaee9b79014e8c6826e052d9f3708f85
Apres Azghaldian <azgaldian2@gmail.com>
2021-10-11 15:07:19 +0300

    creater another 5 function for get from massive: moda, avarage, median, odd numbers, positive numbers

--- HW5/js5.js
+++ HW5/js5.js
@@ -1,24 +1,74 @@
+// 1 getRandomArray
+
 function getRandomArray (length, min, max) {
 	let randomArr = [];
-	    for (let i = 0; i < length; i++) {
-		randomArr.push(Math.floor(Math.random() * (max - min) + min));
+		for (let i = 0; i < length; i++) {
+			randomArr.push(Math.floor(Math.random() * (max - min) + min));
 	}
-	    return randomArr;
+		return randomArr;
 }
 
-console.log('Функція 1:', getRandomArray(10, 3, 99));
+console.log(getRandomArray(10, 3, 99));
 
+// 2 getModa(...numbers)
 
+const getModa = (...numbers) => {
+    const frequency = {};
+    numbers.forEach(number => {
+        if(!frequency[number]){
+            frequency[number] = 1
+        }
+        else{
+            frequency[number] += 1;
+        }
+    });
+    const maxFrequency = Math.max(...Object.values(frequency));
+    const moda = Object.entries(frequency).filter(item => item[1] === maxFrequency);
 
-// getModa(...numbers)
+    return moda.map(number => number[0]).concat();
+}
+console.log(getModa(6, 2, 55, 11, 78, 2, 55, 55, 77, 57, 87, 55, 23, 2, 56, 3, 2));
 
-// getAverage(...numbers)
+// 3 getAverage(...numbers)
 
-// getMedian(...numbers)
+function getAverage(...numbers) {
+	const avarage = numbers.filter(number => Number.isInteger(number))
+		.reduce((sum, number) => (sum + number)) / numbers.length;
+			return avarage
+}
 
-// filterEvenNumbers(...numbers)
+console.log(getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 
-// countPositiveNumbers(...numbers)
+// 4 getMedian(...numbers)
+
+function getMedian(...numbers) {
+	const sortNum = numbers.sort(function(a, b) {
+		return a - b;
+	});
+
+	const minOfMiddle = Math.floor((sortNum.length - 1) / 2);
+	const maxOfMiddle = Math.ceil((sortNum.length - 1) / 2);
+		return (sortNum[minOfMiddle] + sortNum[maxOfMiddle]) / 2;
+}
+
+console.log(getMedian(1, 2, 3, 4, 5));
+
+// 5 filterEvenNumbers(...numbers)
+
+function filterEvenNumbers(...numbers) {
+	let result = numbers.filter(elem => elem % 2 !== 0);
+		return result
+}
+console.log(filterEvenNumbers(1, 2, 3, 4, 5, 6)); 
+
+// 6 countPositiveNumbers(...numbers)
+
+function countPositiveNumbers(...numbers) {
+	const result = numbers.filter(number => number > 0)
+	.sort((a, b) => a - b);
+		return result
+}
+console.log(countPositiveNumbers(1, -2, 3, -4, -5, 6)); 
 
 // getDividedByFive(...numbers)
 
